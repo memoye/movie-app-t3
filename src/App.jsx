5d06324e038505832497836da0b6b50083f09161
Brown Memoye <memoyebrown@gmail.com>
2023-09-19 05:31:29 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,37 @@ import './App.css'
 import NavBar from './components/NavBar'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import { AuthContextProvider } from './context/AuthContext'
+import Login from './pages/Login'
+import Signup from './pages/SignUp'
+import Account from './pages/Account'
+import ProtectedRoute from './components/ProtectedRoute'
+import Details from './pages/Details'
 
 function App() {
 
+
+
   return (
     <>
-      <NavBar />
-      <Router>
-        <Routes>
-          <Route path='/' element={ <Home /> } />
-        </Routes>
-      </Router>
+      <AuthContextProvider>
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path='/' element={ <Home /> } />
+            <Route path='/login' element={ <Login /> } />
+            <Route path='/signup' element={ <Signup /> } />
+
+            <Route path='/details/:id' element={ <Details /> } />
+
+            <Route path='/account' element={ <ProtectedRoute>
+              <Account />
+            </ProtectedRoute> } />
+
+
+          </Routes>
+        </Router>
+      </AuthContextProvider>
     </>
   )
 }
